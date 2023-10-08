c2d42f229c915a0c3f989e81498ba16fa29bc01a
Your Name01~ <you@example.com>
2023-10-08 22:33:20 +0545

    Changes on done

--- src/app/(views)/(Auth)/login/page.jsx
+++ src/app/(views)/(Auth)/login/page.jsx
@@ -14,6 +14,9 @@ import { Divider, Grid, Stack, Typography, useMediaQuery } from "@mui/material";
 // project imports
 import AuthWrapper from "../AuthWrapper";
 import AuthCardWrapper from "../AuthCardWrapper";
+import AuthLogin from "./AuthLogin";
+import Logo from "@/app/components/Logo";
+import AuthFooter from "@/app/components/cards/AuthFooter";
 
 const Login = () => {
   //
@@ -51,18 +54,108 @@ const Login = () => {
   };
 
   return (
-    <Container>
-      <FormContainer>
-        <TitleContainer>Login</TitleContainer>
-        <CustomInput placeholder="Enter Email..." type="text" ref={emailRef} />
-        <CustomInput
-          placeholder="Enter Password..."
-          type="password"
-          ref={passwordRef}
-        />
-        <CustomButton onClick={(e) => submitForm(e)}> Submit </CustomButton>
-      </FormContainer>
-    </Container>
+    <AuthWrapper>
+      <Grid
+        container
+        direction="column"
+        justifyContent="flex-end"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            sx={{ minHeight: "calc(100vh - 68px)" }}
+          >
+            <Grid item sx={{ m: { xs: 1, sm: 3 }, mb: 0 }}>
+              <AuthCardWrapper>
+                <Grid
+                  container
+                  spacing={2}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Grid item sx={{ mb: 3 }}>
+                    <Logo />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid
+                      container
+                      direction={matchDownSM ? "column-reverse" : "row"}
+                      alignItems="center"
+                      justifyContent="center"
+                    >
+                      <Grid item>
+                        <Stack
+                          alignItems="center"
+                          justifyContent="center"
+                          spacing={1}
+                        >
+                          <Typography
+                            color={theme.palette.secondary.main}
+                            gutterBottom
+                            variant={matchDownSM ? "h3" : "h2"}
+                          >
+                            Hi, Welcome Back Manish
+                          </Typography>
+                          <Typography
+                            variant="caption"
+                            fontSize="16px"
+                            textAlign={matchDownSM ? "center" : "inherit"}
+                          >
+                            Enter your credentials to continue
+                          </Typography>
+                        </Stack>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <AuthLogin />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Divider />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid
+                      item
+                      container
+                      direction="column"
+                      alignItems="center"
+                      xs={12}
+                    >
+                      <Typography
+                        // component={Link}
+                        to="/pages/register/register3"
+                        variant="subtitle1"
+                        sx={{ textDecoration: "none" }}
+                      >
+                        Don&apos;t have an account?
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </AuthCardWrapper>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sx={{ m: 3, mt: 1 }}>
+          <AuthFooter />
+        </Grid>
+      </Grid>
+    </AuthWrapper>
+    // <Container>
+    //   <FormContainer>
+    //     <TitleContainer>Login</TitleContainer>
+    //     <CustomInput placeholder="Enter Email..." type="text" ref={emailRef} />
+    //     <CustomInput
+    //       placeholder="Enter Password..."
+    //       type="password"
+    //       ref={passwordRef}
+    //     />
+    //     <CustomButton onClick={(e) => submitForm(e)}> Submit </CustomButton>
+    //   </FormContainer>
+    // </Container>
   );
 };
 
